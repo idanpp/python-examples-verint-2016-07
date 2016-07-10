3155e0b171240ba8d66b41787e1ba9f5f337efd6
idanpp <idan.pinto@verint.com>
2016-07-10 17:37:46 +0300

    Solution for: sol-python-verint.syntax-lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -1,6 +1,10 @@
-"""
-Write a Python program that randomizes 7 numbers
-and prints their sum total.
-If the sum is divisable by 7, also print the word "Boom"
-"""
+from random import randint
+    
+sum = 0
+for x in range(1,7) :
+    num = randint(1,100)
+    sum += num
 
+print sum
+if (sum % 7) == 0 :
+    print "BOOM"
