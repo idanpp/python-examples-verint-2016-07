5aa47fb1038401d45b3541c7fc72cce26255fd9c
idanpp <idan.pinto@verint.com>
2016-07-10 17:37:44 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,5 +1,10 @@
-"""
-Write a program that reads 10 numbers from
-the user and prints the largest one
-"""
 
+print "Please choose Number"
+x = int(raw_input())
+
+for number in range(1,10) :
+    print "choose agian"
+    num = int(raw_input())
+    x = max(num,x)
+
+print "Highes number is" ,x
