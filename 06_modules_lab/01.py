c19d4ba93cab8afa125e2398096e97c612c3473e
idanpp <idan.pinto@verint.com>
2016-07-11 14:44:00 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,6 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
-"""
+import sys 
+
+amount = int(sys.argv[1])
+
+for i in range(0,amount):
+    print "Hello Python"
